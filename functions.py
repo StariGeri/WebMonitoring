8e2568baae9540cc27bf4ab25ff5552a94e45f78
StariGeri <sgergostarosta@gmail.com>
2023-03-17 20:08:32 +0100

    forever loop and main function

--- functions.py
+++ functions.py
@@ -1,5 +1,11 @@
 import smtplib
+import os
 from bs4 import BeautifulSoup
+
+# environment variables for safety
+email_addr = os.environ.get("STARI_DEV_EMAIL_ADDR")
+email_pass = os.environ.get("STARI_DEV_EMAIL_PASS")
+email_to = os.environ.get("STARI_DEV_EMAIL_ADDR2")
 
 
 def htmlTrimmer(string):
@@ -14,13 +20,17 @@ def htmlTrimmer(string):
 
     return str(soup).replace('\r', '')
 
-#get the title of the website
+# get the title of the website
+
+
 def extractTitle(string):
     soup = BeautifulSoup(string, features="lxml")
     return soup.title.string
 
 # send email using gmail SMTP
-def sendMail(emailSubject, email_addr,email_to, email_pass, url):
+
+
+def sendMail(emailSubject, email_addr, email_to, email_pass, url):
     with smtplib.SMTP("smtp.gmail.com", 587) as smtp:
         # encrypt the connection
         smtp.ehlo()
@@ -36,3 +46,32 @@ def sendMail(emailSubject, email_addr,email_to, email_pass, url):
         msg = f"Subject: {subject}\n\n{body}"
 
         smtp.sendmail(email_addr, email_to, msg)
+
+
+def websiteCheck(url, response):
+    print("Checking website... " + url)
+
+    # create file if it does not exist
+    if os.path.exists("lastCheck.html") == False:
+        open("lastCheck.html", 'w+', encoding='utf-8').close()
+
+    # otherwise just open it
+    f = open("lastCheck.html", 'r', encoding='utf-8')
+    lastHtml = f.read()
+    f.close()
+
+    #trim the HTML using the function created above and extract the title
+    trimmedHtml = htmlTrimmer(response.text)
+    title = extractTitle(trimmedHtml).encode(
+        'utf-8').decode('ascii', 'ignore') # remove non-ascii characters
+
+    #check for changes and send email if there are any
+    if trimmedHtml == lastHtml:
+        return False
+
+    else:
+        file = open("lastCheck.html", 'w', encoding='utf-8')
+        file.write(trimmedHtml)
+        file.close()
+        sendMail(title, email_addr, email_to, email_pass, url)
+        return True
--- main.py
+++ main.py
@@ -1,41 +1,42 @@
 import requests
-import os
-
+import time
 import functions
 
 # url to check, it can be changed to any websites url
 url = "https://triathlon.org/events/start_list/2023_world_triathlon_aquathlon_championships_ibiza/584002"
-
-# environment variables for safety
-email_addr = os.environ.get("STARI_DEV_EMAIL_ADDR")
-email_pass = os.environ.get("STARI_DEV_EMAIL_PASS")
-email_to = os.environ.get("STARI_DEV_EMAIL_ADDR2")
+sleepTime = 600 # 600 sec - 10 min
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
            "Pragma": "no-cache", "Cache-Control": "no-cache"}
 
 response = requests.get(url, headers=headers)
 
-# create a new txt file if it doesn't exist already
-if os.path.exists("lastCheck.html") == False:
-    open("lastCheck.html", 'w+', encoding='utf-8').close()
 
-f = open("lastCheck.html", 'r', encoding='utf-8')
-lastHtml = f.read()
-f.close()
+def main():
+    #forever loop on purpose, to check the website every {sleepTime} seconds
+    #uncomment it to run it forever
+    '''while True:
+        try:
+            if(response.status_code == 200) == True:
+                if (functions.websiteCheck(url, response)) == True:
+                    print("Website is up and changed! Email sent!")
+                else:
+                    print("Website is up and not changed!")    
 
-# trim the html using htmlTrim()
-trimmedHtml = functions.htmlTrimmer(response.text)
-title = functions.extractTitle(trimmedHtml).encode('utf-8').decode('ascii', 'ignore')
+        except:
+            print("Website is down, cannot check")   
+        time.sleep(sleepTime)'''
+        
+    try:
+        if(response.status_code == 200) == True:
+            if (functions.websiteCheck(url, response)) == True:
+                print("Website is up and changed! Email sent!")
+            else:
+                print("Website is up and not changed!")
+    except:
+        print("Website is down, cannot check")
 
-# check if the html is changed or not
-# for the first time it will always be changed
-if trimmedHtml == lastHtml:
-    print("Website did not change!")
 
-else:
-    file = open("lastCheck.html", 'w', encoding='utf-8')
-    file.write(trimmedHtml)
-    file.close()
-    functions.sendMail(title, email_addr, email_to, email_pass, url)
-    print("Website Changed! Email sent!")
+# glue everything together
+if __name__ == "__main__":
+    main()
